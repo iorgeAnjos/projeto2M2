c918fe2f2ca13c89e0782266344b66b857c5e22c
iorgeAnjos <jorgez.anjos@gmail.com>
2022-06-01 17:12:50 -0300

    commit port

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require('dotenv').config;
 const express = require('express');
 const res = require('express/lib/response');
 const { send } = require('express/lib/response');
 const path = require('path');
 const app = express();
+
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,33 +17,36 @@ const pokedex = [
     nome: 'Zoroark',
     img: 'img/zoroark.png',
     tipo: 'Dark',
-    descricao: 'This Pokémon cares deeply about others of its kind, and it will conjure terrifying illusions to keep its den and pack safe.',
+    descricao:
+      'This Pokémon cares deeply about others of its kind, and it will conjure terrifying illusions to keep its den and pack safe.',
     altura: '1.6 m',
     peso: '81.1 kg',
     categoria: 'Illusion Fox',
-    habilidade: 'Illusion'
+    habilidade: 'Illusion',
   },
   {
     id: 2,
     nome: 'Zarude',
     img: 'img/zarude.png',
     tipo: 'Dark',
-    descricao: "Within dense forests, this Pokémon lives in a pack with others of its kind. It's incredibly aggressive, and the other Pokémon of the forest fear it.",
+    descricao:
+      "Within dense forests, this Pokémon lives in a pack with others of its kind. It's incredibly aggressive, and the other Pokémon of the forest fear it.",
     altura: '1.8 m',
     peso: '70.0 kg',
     categoria: 'Rogue Monkey',
-    habilidade: 'Leaf Guard'
+    habilidade: 'Leaf Guard',
   },
   {
     id: 3,
     nome: 'Urshifu',
     img: 'img/urshifu.png',
     tipo: 'Dark',
-    descricao: 'This form of Urshifu is a strong believer in the one-hit KO. Its strategy is to leap in close to foes and land a devastating blow with a hardened fist.',
+    descricao:
+      'This form of Urshifu is a strong believer in the one-hit KO. Its strategy is to leap in close to foes and land a devastating blow with a hardened fist.',
     altura: '1.9 m',
     peso: '105.0 kg',
     categoria: 'Wushu',
-    habilidade: 'Unseen Fist'
+    habilidade: 'Unseen Fist',
   },
 ];
 let pokemon = undefined;
@@ -72,7 +78,6 @@ app.post('/update/:id', (req, res) => {
   res.redirect('/');
 });
 
-
-app.listen(3000, () =>
-  console.log('Servidor rodando em: http://localhost:3000'),
+app.listen(port, () =>
+  console.log(`Servidor rodando em: http://localhost:${port}`),
 );
